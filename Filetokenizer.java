d26181d0586499eba659a3187c792e3fd3b4e886
pranathiR <pranathigoudi@gmail.com>
2017-04-06 18:44:47 +0530

    Update Filetokenizer.java

--- Filetokenizer.java
+++ Filetokenizer.java
@@ -1,17 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sample;
-
-/**
- *
- * @author welcome
- */
 public class Filetokenizer {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
-    
-}
+    }
